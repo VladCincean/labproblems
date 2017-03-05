8e5b6d0b7fd5b0e710a05fa3881d5a8780af33e0
mjolnir\stefana <gsie1906@scs.ubbcluj.ro>
2017-03-05 17:48:28 +0200

    assignment service

---
+++ src/main/java/ro/droptable/labproblems/service/AssignmentService.java
@@ -0,0 +1,13 @@
+package ro.droptable.labproblems.service;
+
+import ro.droptable.labproblems.domain.Assignment;
+import ro.droptable.labproblems.repository.Repository;
+
+/**
+ * Created by stefana on 3/5/2017.
+ */
+public class AssignmentService extends Service<Assignment> {
+    public void AssignmentService(Repository<Long, Assignment > repository) {
+        this.repository = repository;
+    }
+}
