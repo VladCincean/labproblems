343256c52b1e4ebb0ca8c61255f95cd90a4e4502
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-03-05 17:10:23 +0200

    AssignmentValidator

--- src/main/java/ro/droptable/labproblems/domain/validators/AssignmentValidator.java
+++ src/main/java/ro/droptable/labproblems/domain/validators/AssignmentValidator.java
@@ -6,4 +6,13 @@ import ro.droptable.labproblems.domain.Assignment;
  * Created by vlad on 05.03.2017.
  */
 public class AssignmentValidator implements Validator<Assignment> {
+
+    public AssignmentValidator() {
+    }
+
+    @Override
+    public void validate(Assignment entity) throws ValidatorException {
+        if (!(entity.getGrade() == 0.0 || (1.0 <= entity.getGrade() && entity.getGrade() <= 10.0)))
+            throw new ValidatorException("assignment grade must be 0 or between 1 and 10");
+    }
 }
