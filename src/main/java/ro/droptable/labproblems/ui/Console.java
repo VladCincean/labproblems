89032c6f1d67a7348908d0cf8e2ef66e8a0e90d2
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-03-05 11:59:56 +0200

    in memory repo + started working on ui

--- src/main/java/ro/droptable/labproblems/ui/Console.java
+++ src/main/java/ro/droptable/labproblems/ui/Console.java
@@ -1,7 +1,18 @@
 package ro.droptable.labproblems.ui;
 
+import ro.droptable.labproblems.service.StudentService;
+
 /**
  * Created by vlad on 04.03.2017.
  */
 public class Console {
+    private StudentService studentService;
+//    private ProblemService problemService;
+//    private AssignmentService assignmentService;
+
+    public Console(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // TODO: implement the rest
 }
