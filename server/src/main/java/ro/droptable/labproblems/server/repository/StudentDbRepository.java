4fc20acfc78bf807ee2ba6b3896d622fd2cfa7ed
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-03-29 19:57:06 +0300

    deleting all student's/problem's assignments within the db-repository

--- server/src/main/java/ro/droptable/labproblems/server/repository/StudentDbRepository.java
+++ server/src/main/java/ro/droptable/labproblems/server/repository/StudentDbRepository.java
@@ -132,6 +132,7 @@ public class StudentDbRepository implements Repository<Long, Student> {
         }
     }
 
+    @SuppressWarnings("Duplicates")
     @Override
     public Optional<Student> delete(Long id) {
         if (id == null) {
@@ -144,11 +145,18 @@ public class StudentDbRepository implements Repository<Long, Student> {
         }
 
         try (Connection connection = DriverManager.getConnection(url, username, password);
-             PreparedStatement statement = connection.prepareStatement("DELETE FROM students WHERE id=?"))
+             PreparedStatement deleteStudentStatement = connection.prepareStatement(
+                     "DELETE FROM students WHERE id=?"
+             );
+             PreparedStatement deleteAssignmentsStatement = connection.prepareStatement(
+                     "DELETE FROM assignments where student_id=?"
+             ))
         {
-            statement.setLong(1, id);
+            deleteStudentStatement.setLong(1, id);
+            deleteAssignmentsStatement.setLong(1, id);
 
-            statement.executeUpdate();
+            deleteAssignmentsStatement.executeUpdate();
+            deleteStudentStatement.executeUpdate();
 
             return student;
         } catch (SQLException e) {
